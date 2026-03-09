3e468d4117f571411a245e80f5dacac127196611
Aditya Kakde <adityakakde@gmail.com>
2026-03-09 21:47:05 +0530

    backend notes api added

--- day-8/Backend/src/app.js
+++ day-8/Backend/src/app.js
@@ -2,10 +2,82 @@
 * server ko create krne ke liye
 */
 
-
+const noteModel = require("./models/note.model")
 const express = require ("express")
 
 const app = express()
+app.use(express.json())
+
+/**
+ *  - POST /api/notes
+ * -create new note and save data in mongodb
+ * - req.body = {title,description}
+ */
+
+app.post('/api/notes', async (req,res) =>{
+    const { title,description } = req.body
+
+  const note = await noteModel.create({
+    title, description
+   })
+
+   res.status(201).json({
+    message:"note created successfully",
+    note
+
+   })
+})
 
 
+
+/**
+ * -GET/api/notes
+ * 
+ * - Fetch all the note data from mongodb and send them into response*/
+
+app.get("/api/notes", async (req, res) =>{
+  const notes = await noteModel.find()
+
+  res.status(200).json({
+    message:"notes fetched sucessfully. ",notes
+  })
+})
+
+
+
+
+/**
+ * -DELETE /api/notes/:id
+ * -delte note  with the it from  req.params
+ */
+
+
+app.delete("/api/notes/:id",async (req,res)=>{
+  const id = req.params.id
+
+  await noteModel.findByIdAndDelete(id)
+
+  res.status(200).json({
+    message:"notes deleted successfully."
+  })
+  
+})
+
+
+/**
+ * PATCH /api/notes/:id
+ * update the description of the note by id
+ * req.body ={description}
+ */
+
+app.patch("/api/notes/:id", async (req,res)=>{
+  const id = req.params.id
+  const{description} = req.body
+
+  await noteModel.findByIdAndUpdate(id,{description})
+
+  res.status(200).json({
+    message:"notes updated successfully. "
+  })
+})
 module.exports = app
