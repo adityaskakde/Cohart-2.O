52a91fbd191ddddf274814a4cfa23204bc795f57
Aditya Kakde <adityakakde@gmail.com>
2026-03-10 00:08:33 +0530

    updated frontend

--- day-8/Backend/src/app.js
+++ day-8/Backend/src/app.js
@@ -1,11 +1,13 @@
 /*
 * server ko create krne ke liye
 */
-
-const noteModel = require("./models/note.model")
 const express = require ("express")
+const noteModel = require("./models/note.model")
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 /**
