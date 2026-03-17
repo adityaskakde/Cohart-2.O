183a728d489700d133e7cd43aabf3388aeb7f8fa
Aditya Kakde <adityakakde@gmail.com>
2026-03-17 21:35:00 +0530

    post with token

--- Day-10/src/controllers/post.controller.js
+++ Day-10/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile,  } = require("@imagekit/nodejs")
+const jwt = require ('jsonwebtoken')
 
 
 
@@ -13,16 +14,44 @@ const imagekit = new ImageKit({
 async function createPostController(req, res) {
     console.log(req.body, req.file);
 
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:"Token not provided ,unauthorized access"
+        })
 
+    }
+let decoded;
+
+try{
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+     
+}catch (err){
+    return res.status(401).json({
+        message:"User not authorized"
+    })
+
+}
+      console.log(decoded);
+      
+
+    
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer),'file'),
-        fileName:"Test"
-
+        fileName:"Test",
+        folder: "cohort2/insta_clone_posts"
     })
-    res.send(file)
+    const post = await postModel.create({
+        caption:req.body.caption,
+        imgUrl:file.url,
+        user:decoded.id
+    })
     
 
-
+res.status(201).json({
+    message:"Post Created successfully.",
+    post
+})
 
 
 }
