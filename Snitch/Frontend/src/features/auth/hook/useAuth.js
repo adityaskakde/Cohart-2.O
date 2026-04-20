6a21a4df88b9173552a04de9bcef87c258eeb5fa
Aditya Kakde <adityakakde@gmail.com>
2026-04-20 18:50:26 +0530

    first commit

---
+++ Snitch/Frontend/src/features/auth/hook/useAuth.js
@@ -0,0 +1,20 @@
+import { setError,setUser,setLoading } from "../state/auth.slice";
+import { register } from "../services/auth.api";
+import { useDispatch } from "react-redux";
+
+
+export const useAuth = ()=>{
+    const dispatch = useDispatch();
+
+    async function handleRegister({email,contact,password,fullname,isSeller = false}){
+     
+       const data = await register({email,contact,password,fullname,isSeller});
+       
+       dispatch(setUser(data.user));
+    }
+
+
+
+
+    return {handleRegister}
+}
